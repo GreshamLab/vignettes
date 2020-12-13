6daf1ac91972aec25d6edcc2de643c02c70e0f7c
davidgresham <dg107@nyu.edu>
2020-12-13 14:16:43 -0500

    progress on gating using cytoexplorer

--- docs/CytoExploreR.R
+++ docs/CytoExploreR.R
@@ -22,16 +22,17 @@ library(tidyverse)
 
 #Install CytoExplorer package and requirements (can be skipped if already installed)
 #library(BiocManager)
-#install(“cytolib”, “flowCore”, “flowWorkspace”, “openCyto”)
+#1
+install("cytolib", "flowCore", "flowWorkspace", "openCyto")
 
 #Then you can install CytoExploreR from GitHub:
 
-#devtools::install_github("DillonHammill/CytoExploreRData")
-#devtools::install_github("DillonHammill/CytoExploreR")
+devtools::install_github("DillonHammill/CytoExploreRData")
+devtools::install_github("DillonHammill/CytoExploreR")
 
 # Load required packages
 library(CytoExploreR)
-library(CytoExploreRData)
+#library(CytoExploreRData)
 
 #Setup experiment using cyto_setup()
 #This function will read .fcs files to a cutoset which are then added to a GatingSet
@@ -41,6 +42,20 @@ accuri_gating_set <- cyto_setup(path="/Users/david/Projects/data/flowdata/Accuri
 
 #To interactively edit the experiment details
 cyto_details_edit(accuri_gating_set)
+
+
+cyto_plot_explore(accuri_gating_set,
+                  channels_x = "FSC-A",
+                  channels_y = c("GFP")
+)
+
+
+cyto_plot_profile(accuri_gating_set,
+                  parent = "root",
+                  channels = c("FSC-A","GFP")
+                  )
+
+cyto_plot_gating_tree(accuri_gating_set)
 
 #Transform the data using a logicle transformation.
 #The first step applies the transformation using cyto_transformer_logicle()
@@ -65,6 +80,7 @@ transformed_accuri <- cyto_transform(accuri_gating_set,
 #individual files can be called as well
 
 #The first gate defines cells based on forward scatter and side scatter
+#For this gate we use all the cells
 cyto_gate_draw(transformed_accuri,
                parent = "root",
                alias = "Cells",
@@ -82,57 +98,108 @@ cyto_gate_draw(transformed_accuri,
                gatingTemplate = "Accuri_gating.csv"
                )
 
+cyto_plot_gating_scheme(transformed_accuri)
+cyto_plot_gating_tree(transformed_accuri, stat="freq")
+cyto_plot_gating_scheme(transformed_accuri[3], stat="freq")
 #####################
 
 #To generate the gates based on individuals samples, we need to use the appropriate individual samples
 
 #Identify the negative control/no GFP sample and use it to gate non-fluorescent cells
-cyto_gate_draw(transformed_accuri[1],
+cyto_gate_draw(transformed_accuri,
                parent = "Single_cells",
                alias = "No GFP",
                channels = c("FSC-A","FL1-A"),
                axes_limits = "data",
-               gatingTemplate = "Accuri_gating2.csv"
+               select = list(Strain = "DGY1"),  #strain used to define no GFP signal
+               gatingTemplate = "Accuri_gating.csv"
                )
 
-cyto_gate_edit(transformed_accuri,
-               parent = "Single_cells",
-               alias = "No GFP",
-               channels = c("FSC-A","FL1-A"),
-               axes_limits = "data",
-               gatingTemplate = "Accuri_gating.csv"
-)
+cyto_plot_gating_scheme(transformed_accuri[1:2], stat="freq")
 
-cyto_gate_draw(transformed_accuri[2],
-               parent = "Single_cells",
-               alias = "One copy GFP",
-               channels = c("FSC-A","GFP"),
-               axes_limits = "data",
-               gatingTemplate = "Accuri_gating.csv"
-)
 
-cyto_gate_draw(transformed_accuri[3],
-               parent = "Single_cells",
-               alias = "Two copy GFP",
-               channels = c("FSC-A","GFP"),
-               axes_limits = "data",
-               gatingTemplate = "Accuri_gating.csv"
-)
-
-###in order to visualize an existing gate overlaied on cells
-
-#first we need to extract the cells to be overlaied
+##To overlay the No GFP cells when gating the single copy GFP
 negative <- cyto_extract(transformed_accuri, "Single_cells")[[1]]
 
-#the overlay argument plots the cells as gray cells
-cyto_gate_draw(transformed_accuri[3],
+
+#Define the one copy GFP gate using the relevant control sample
+cyto_gate_draw(transformed_accuri,
                parent = "Single_cells",
-               alias = c("Neg","Two"),
-               channels = c("FSC-A","GFP"),
+               alias = "One copy GFP",
+               channels = c("FSC-A","FL1-A"),
+               axes_limits = "data",
+               select = list(Strain = "DGY500"),  #strain used to define one copy of GFP
+               gatingTemplate = "Accuri_gating.csv",
+               overlay=negative  #will plot the negative cells as gray plots on the same plot
+)
+
+cyto_plot_gating_scheme(transformed_accuri[1:3], stat="freq")
+
+##To overlay the one GFP copy cells when gating the two copy GFP
+one_copy <- cyto_extract(transformed_accuri, "Single_cells")[[2]]
+
+#Define the two copy GFP gate using the relevant control sample
+cyto_gate_draw(transformed_accuri,
+               parent = "Single_cells",
+               alias = "Two copy GFP",
+               channels = c("FSC-A","FL1-A"),
+               select = list(Strain = "DGY1315"),  #strain used to define two copies of GFP
                axes_limits = "data",
                gatingTemplate = "Accuri_gating.csv",
-               overlay=negative
+               overlay=one_copy  #will plot the one copy cells as gray points on the same plot for reference
 )
+
+cyto_plot_gating_scheme(transformed_accuri[4], stat="freq")
+
+##To overlay the one GFP copy cells when gating the two copy GFP
+two_copy <- cyto_extract(transformed_accuri, "Single_cells")[[3]]
+
+#Define the three copy GFP gate using the relevant control sample
+cyto_gate_draw(transformed_accuri,
+               parent = "Single_cells",
+               alias = "More than two copies GFP",
+               channels = c("FSC-A","FL1-A"),
+             #  select = list(name = "DGY2158"),
+               axes_limits = "data",
+               gatingTemplate = "Accuri_gating.csv",
+               overlay=two_copy  #will plot the two copy cells as gray points on the same plot for reference
+)
+
+
+cyto_plot_gating_scheme(transformed_accuri, stat="freq")
+
+cyto_plot_gating_tree(transformed_accuri[[7]],
+                      stat="freq")
+
+cyto_plot_gating_scheme(transformed_accuri[1],
+                        back_gate = TRUE,
+                        gate_track = TRUE)
+
+stats <- cyto_stats_compute(transformed_accuri,
+                   parent = "Single_cells",
+                   alias = c("No GFP", "One copy GFP", "Two copy GFP", "More than two copies GFP"),
+                   stat="freq")
+
+View(stats)
+
+#############Drawing multiple gates simultaneously.
+
+#It is possible to draw multiple gates by defining them
+
+
+cyto_gate_draw(transformed_accuri,
+               parent = "Single_cells",
+               alias = c("Neg", "One", "Two","More"), #defines gate names (4 total in this case)
+               channels = c("FSC-A","FL1-A"),
+               axes_limits = "data",
+               select = list(Strain = c("DGY1","DGY500","DGY1315","DGY2158")),  #control strains used to different copy numbers of GFP
+               gatingTemplate = "Accuri_gating.csv",
+               overlay = c(negative, one_copy, two_copy),  #the corresponding data for each control which has been extracted
+               point_col = c("black", "green", "red", "blue")
+               )
+
+
+#############Additional
 
 #if you need to redraw a gate you need to use cyto_gate_edit()
 cyto_gate_edit(transformed_accuri,
@@ -141,20 +208,54 @@ cyto_gate_edit(transformed_accuri,
                gatingTemplate = "Accuri_test_data.csv",
                type = "boundary")
 
-
 ###Drawing a tree of the gated samples
-cyto_plot_gating_tree(transformed_accuri[[3]],
+cyto_plot_gating_tree(transformed_accuri,
                       stat="freq")
 
 
-#Draw the gating scheme
+#Draw the gating scheme for a single sample
 cyto_plot_gating_scheme(transformed_accuri[[3]],
                         back_gate = TRUE,
                         gate_track = TRUE)
 
 
-cyto_stats_compute(transformed_accuri[2],
+cyto_stats_compute(transformed_accuri,
                    alias = c("One copy GFP"),
-                   stet - "median",
+                   stat = "median",
                    channels = "GFP")
+
+
+#############Visualizing data
+
+
+cyto_plot_explore(transformed_accuri,
+                  channels_x = "FSC-A",
+                  channels_y = "GFP"
+)
+
+
+cyto_plot(transformed_accuri,
+          parent="Cells",
+          channels = "FSC-A",
+          alias = "",
+          xlim = c(10000, 2000000),
+          density_fill = rep("blue",19),
+          density_stack = 0.7)
+
+cyto_plot(transformed_accuri[1:19],
+          parent="Cells",
+          channels = "GFP",
+        #  alias = c("One copy GFP"),
+          alias = "",
+          xlim = c(10000, 2000000),
+          density_fill = rep("green",19),
+          density_stack = 0.7)
+
+cyto_plot(transformed_accuri[2],
+          parent="Cells",
+          alias = c("Neg", "One", "Two", "More"),
+          channels = c("FSC-A","GFP"),
+          xlim = c(100000, 3000000),
+          ylim = c(10000, 3000000),
+      )
 
